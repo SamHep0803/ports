ef4b61130d691a5f23f4c361e0189ab18ed3a96f
Samuel Hepworth <samhep0803@gmail.com>
2026-02-17 02:00:07 +0000

    feat: add cobra

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ports",
+	Short: "Ports is an SSH port forward manager.",
+	Long: `An SSH port forwarding manager built by
+					SamHep0803 in go.`,
+	// Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ports.yaml)")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/samhep0803/ports/cmd"
+
+func main() {
+	cmd.Execute()
+}
